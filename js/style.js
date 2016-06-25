4e45eb4b69b4777786b2f35a25cd77e37de17e3c
Phillmungah <philipnjoroge66520@gmail.com>
2016-06-25 07:10:04 +0300

    firstcommit

---
+++ js/style.js
@@ -0,0 +1,10 @@
+var white = document.getElementsByClassName("oranges").oranges[0];
+var brown = document.getElementById("more");
+
+
+
+function black() {
+    white.style.visibility = "visible";
+}
+
+brown.onclick = black();
